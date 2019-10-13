88a5d11803fbd4cb03b17ea9a372c68487fce520
Steven Thomas <stevent3115@gmail.com>
2019-10-13 16:52:12 -0400

    Added support for all 'question' words (who, what, when, where, why, how) to KeywordsExtractor.

--- whatwhy/keywords_extractor.py
+++ whatwhy/keywords_extractor.py
@@ -5,69 +5,82 @@ from Giveme5W1H.extractor.document import Document
 from Giveme5W1H.extractor.extractor import MasterExtractor
 import data_cleaner
 
-# TODO
-# Consider aggregating "what", "who", "when", "where" words
-# from Giveme5W1H into one "what" category, and aggregating
-# "why" and "how" words into one "why" category.
+_5W1H_WORDS = ["Who", "What", "When", "Where", "Why", "How"]
+
 class KeywordsExtractor():
-    """Class for extracting the 'what' and 'why' keywords from a text column in a pandas DataFrame."""
+    """
+    Class for extracting the 5w1h keywords ('who', 'what', 'when', 'where', 'why', 'how')
+    from a text column in a pandas DataFrame.
+    """
 
     def __init__(self, df):
-        self.df = df
-        self.what_and_why_extractor = MasterExtractor()
+        self._df = df
+        self._5w1h_extractor = MasterExtractor()
 
-    def add_what_and_why_keywords_to_df(self):
+    def add_5w1h_keywords_to_df(self):
         self.preprocess_text()
-        self.add_full_what_and_why_texts_to_df()
-        self.normalize_what_and_why_columns()
-        self.filter_words_from_what_and_why_lists()
+        self.initialize_5w1h_columns()
+        self.add_full_5w1h_texts_to_df()
+        self.normalize_5w1h_columns()
+        self.filter_words_from_5w1h_lists()
         logging.info("Done.")
-        return self.df
+        return self._df
 
     def preprocess_text(self):
         """Removes Twitter @reply tags and autocorrects spelling and grammar."""
         logging.info("Preprocessing text...")
-        self.df["Text"] = self.df["Text"].map( data_cleaner.remove_reply_tag_from_tweet_text ) \
+        self._df["Text"] = self._df["Text"].map( data_cleaner.remove_reply_tag_from_tweet_text ) \
                                          .map( data_cleaner.autocorrect_spelling_and_grammar )
         
-    def add_full_what_and_why_texts_to_df(self, column="Text"):
-        """Extracts the 'what' and 'why' phrases from all text using Giveme5W1H."""
-        logging.info("Extracting what/why phrases from text...")
-        self.df["What"] = None
-        self.df["Why"] = None  
-        for id, row in self.df.iterrows():
+    def add_full_5w1h_texts_to_df(self, column="Text"):
+        """Extracts the 5w1h phrases from all text."""
+        
+        def get_5w1h_phrase_or_empty_string(doc, question_type):
+            try:
+                phrase = doc.get_top_answer( question_type.lower() ).get_parts_as_text()
+                if phrase is None:
+                    phrase = ""
+                return phrase
+            except:
+                return ""
+
+        logging.info("Extracting 5w1h phrases from text...")
+        for id, row in self._df.iterrows():
             try:
                 text = row[column]
                 if text is None or text is np.nan:
                     continue
                 doc = Document.from_text(text)
-                doc = self.what_and_why_extractor.parse(doc)            
-                what = doc.get_top_answer('what').get_parts_as_text()   
-                why = doc.get_top_answer('why').get_parts_as_text()
-                self.df.at[id, "What"] = what        
-                self.df.at[id, "Why"] = why
-            except:
+                doc = self._5w1h_extractor.parse(doc)
+                for question_type in _5W1H_WORDS:
+                    self._df.at[id, question_type] = get_5w1h_phrase_or_empty_string(doc, question_type)
+            except Exception as e:
+                logging.debug(e)
                 continue
 
-    def normalize_what_and_why_columns(self):
-        """Converts the raw what/why texts to lists of lowercase and lemmatized words."""
-        logging.info("Normalizing and tokenizing what/why text...")
-        self.convert_what_and_why_columns_to_lowercase()
-        self.tokenize_and_lemmatize_what_and_why_columns()
+    def initialize_5w1h_columns(self):
+        for column_name in _5W1H_WORDS:
+            self._df[column_name] = None
 
-    def convert_what_and_why_columns_to_lowercase(self):
-        self.df["What"] = self.df["What"].str.lower()
-        self.df["Why"] = self.df["Why"].str.lower()
+    def normalize_5w1h_columns(self):
+        """Converts the raw 5w1h texts to lists of lowercase and lemmatized words."""
+        logging.info("Normalizing and tokenizing 5w1h text...")
+        self.convert_5w1h_columns_to_lowercase()
+        self.tokenize_and_lemmatize_5w1h_columns()
 
-    def tokenize_and_lemmatize_what_and_why_columns(self):
-        self.df["What"] = self.df["What"].map( data_cleaner.get_list_of_lemmatized_words_from_text )
-        self.df["Why"] = self.df["Why"].map( data_cleaner.get_list_of_lemmatized_words_from_text )
+    def convert_5w1h_columns_to_lowercase(self):
+        for column_name in _5W1H_WORDS:
+            self._df[column_name] = self._df[column_name].str.lower()
 
-    def filter_words_from_what_and_why_lists(self):
-        """Extracts the most important keywords from lists of words in the what/why columns."""
-        logging.info("Extracting keywords from what/why text...")
-        self.df["What"] = self.df["What"].map( self.filter_words_from_list )
-        self.df["Why"] = self.df["Why"].map( self.filter_words_from_list )
+    def tokenize_and_lemmatize_5w1h_columns(self):
+        for column_name in _5W1H_WORDS:
+            self._df[column_name] = self._df[column_name].map( data_cleaner.get_list_of_lemmatized_words_from_text )
+
+    def filter_words_from_5w1h_lists(self):
+        """Extracts the most important keywords from lists of words in the 5w1h columns."""
+        logging.info("Extracting keywords from 5w1h text...")
+        for column_name in _5W1H_WORDS:
+            self._df[column_name] = self._df[column_name].map( self.filter_words_from_list )
 
     def filter_words_from_list(self, words):
         """Extracts the most important keywords from a list of words."""
--- whatwhy/main.py
+++ whatwhy/main.py
@@ -8,7 +8,7 @@ def get_df_with_keywords_from_csv(filename, nrows=None):
     logging.info("Loading CSV file...")
     df = pd.read_csv(filename, nrows=nrows)
     extractor = KeywordsExtractor(df)
-    df_with_keywords = extractor.add_what_and_why_keywords_to_df()
+    df_with_keywords = extractor.add_5w1h_keywords_to_df()
     return df_with_keywords
 
 #tweets_csv_file = "/home/zach/git/whatwhy/Tweets/all_tweets_aggregated.csv"
