2a227592587ad2094274c84c0ec03fefcf3ee98d
Steven Thomas <stevent3115@gmail.com>
2019-10-28 00:48:25 -0400

    Added file-system batch source/destination classes to text_processing module.

--- whatwhy/text_processing/__init__.py
+++ whatwhy/text_processing/__init__.py
@@ -1,3 +1,4 @@
 from .text_processing import BatchProcessorBase
 from .s3_client import S3BatchSource, S3BatchDestination
 from .sqs_client import SQSBatchSource, SQSBatchDestination
+from .fs_client import FileSystemBatchSource, FileSystemBatchDestination
