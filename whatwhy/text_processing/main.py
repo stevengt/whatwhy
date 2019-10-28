23d79d954d3cea18e4f17ca1426ce2d1d2ac5a1e
Steven Thomas <stevent3115@gmail.com>
2019-10-28 13:47:16 -0400

    Added delete-when-complete flag to CLI.

--- whatwhy/text_processing/main.py
+++ whatwhy/text_processing/main.py
@@ -6,19 +6,19 @@ from .sqs_client import SQSBatchSource, SQSBatchDestination
 from .preprocessing import BatchPreprocessor
 from .wh_phrases import WHPhrasesBatchProcessor
 
-def get_batch_source(source_type, source_name):
-    if source_type == "file-system":
-        return FileSystemBatchSource(source_name)
+def get_batch_source(source_type, source_name, delete_when_complete):
+    if source_type == "fs":
+        return FileSystemBatchSource(source_name, delete_when_complete=delete_when_complete)
     elif source_type == "s3":
         source_names = source_name.split("/")
-        return S3BatchSource(bucket_name=source_names[0], folder_name=source_names[1])
+        return S3BatchSource(bucket_name=source_names[0], folder_name=source_names[1], delete_when_complete=delete_when_complete)
     elif source_type == "sqs":
         return SQSBatchSource(source_name)
     else:
         raise AttributeError(f"Unsupported batch source type {source_type}.")
 
 def get_batch_destination(dest_type, dest_name):
-    if dest_type == "file-system":
+    if dest_type == "fs":
         return FileSystemBatchDestination(dest_name)
     elif dest_type == "s3":
         dest_names = dest_name.split("/")
@@ -50,23 +50,25 @@ def main():
     arggroup.add_argument("--populate", action="store_true")
     arggroup.add_argument("--process", choices=["preprocessing", "wh-phrases"] )
 
-    parser.add_argument("--source-type", choices=["file-system", "s3", "sqs"], required=True)
+    parser.add_argument("--source-type", choices=["fs", "s3", "sqs"], required=True)
     parser.add_argument("--source-name", required=True, help="If using S3, use the format bucket-name/folder-name.")
-    parser.add_argument("--dest-type", choices=["file-system", "s3", "sqs"], required=True)
+    parser.add_argument("--dest-type", choices=["fs", "s3", "sqs"], required=True)
     parser.add_argument("--dest-name", required=True, help="If using S3, use the format bucket-name/folder-name.")
 
+    parser.add_argument("--delete-when-complete", default=False, action="store_true")
     parser.add_argument("--batch-size", type=int, default=1000)
 
     args = parser.parse_args()
 
     batch_dest = get_batch_destination(args.dest_type, args.dest_name)
     batch_size = args.batch_size
+    delete_when_complete = args.delete_when_complete
 
     if args.populate:
         df_file_name = args.source_name
         populate(df_file_name, batch_dest, batch_size)
     else:
-        batch_source = get_batch_source(args.source_type, args.source_name)
+        batch_source = get_batch_source(args.source_type, args.source_name, delete_when_complete)
         batch_processor_type = args.process
         batch_processor = get_batch_processor(batch_processor_type, batch_source, batch_dest)
         process(batch_processor)
