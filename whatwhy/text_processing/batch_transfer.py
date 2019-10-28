285133ef33e7243e535313f49bac12d2a16e12fd
Steven Thomas <stevent3115@gmail.com>
2019-10-28 14:38:50 -0400

    Added index column names to read_csv() methods.

--- whatwhy/text_processing/batch_transfer.py
+++ whatwhy/text_processing/batch_transfer.py
@@ -1,4 +1,4 @@
-from .text_processing import BatchProcessorBase
+from .text_processing import BatchProcessorBase, get_df_from_csv_string
 
 class BatchTransferer(BatchProcessorBase):
 
@@ -6,4 +6,9 @@ class BatchTransferer(BatchProcessorBase):
         super().__init__(source, dest)
     
     def get_batch_results(self, batch):
+        batch_as_df = get_df_from_csv_string(batch)
+        results = {
+            "target_results_file_name" : f"batch{batch_as_df.index.iloc[0]}.csv",
+            "file_content" : results_csv_string
+        }
         return batch
--- whatwhy/text_processing/text_processing.py
+++ whatwhy/text_processing/text_processing.py
@@ -8,15 +8,15 @@ logger = logging.getLogger(__name__)
 
 def get_csv_string_from_df(df):
     with StringIO() as csv_stream:
-        df.to_csv(csv_stream, sep="\t", index=False, quoting=csv.QUOTE_ALL, quotechar='"')
+        df.to_csv(csv_stream, sep="\t", quoting=csv.QUOTE_ALL, quotechar='"')
         return csv_stream.getvalue()
 
 def get_df_from_csv_string(csv_string):
     with StringIO(csv_string) as csv_stream:
-        return pd.read_csv(csv_stream, sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
+        return pd.read_csv(csv_stream, index_col="ID", sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
 
 def get_df_from_file(file_name):
-    return pd.read_csv(file_name, sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
+    return pd.read_csv(file_name, index_col="ID", sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
 
 class BatchSourceBase():
 
