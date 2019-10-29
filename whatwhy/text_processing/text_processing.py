bf0b4f76c08804b79d6ab399f482abaee6531b97
Steven Thomas <stevent3115@gmail.com>
2019-10-29 13:41:16 -0400

    Removed index_col name from get_df() methods.

--- whatwhy/text_processing/text_processing.py
+++ whatwhy/text_processing/text_processing.py
@@ -13,10 +13,10 @@ def get_csv_string_from_df(df):
 
 def get_df_from_csv_string(csv_string):
     with StringIO(csv_string) as csv_stream:
-        return pd.read_csv(csv_stream, index_col="ID", sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
+        return pd.read_csv(csv_stream, index_col=False, sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
 
 def get_df_from_file(file_name):
-    return pd.read_csv(file_name, index_col="ID", sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
+    return pd.read_csv(file_name, index_col=False, sep="\t", dtype=str, quoting=csv.QUOTE_ALL, quotechar='"')
 
 class BatchSourceBase():
 
