05dce222f89d2e4c4a20a72ed56b863b0c654206
Steven Thomas <stevent3115@gmail.com>
2019-11-11 17:17:31 -0500

    Finalize model training settings.

--- whatwhy/data_analysis/main.py
+++ whatwhy/data_analysis/main.py
@@ -9,8 +9,12 @@ from .vocab_index import VocabularyIndex
 # --------------------------------------
 
 csv_file_name = "/home/stevengt/Documents/code/whatwhy-data/News-Articles/all-the-news/wh_phrases.csv"
-vectorizers_dir = "/home/stevengt/Documents/code/whatwhy-data/News-Articles/all-the-news/vectorizers"
-model_dir = "/home/stevengt/Documents/code/whatwhy-data/News-Articles/all-the-news/tf-model"
+
+# vectorizers_dir = "/home/stevengt/Documents/code/whatwhy-data/News-Articles/all-the-news/vectorizers"
+vectorizers_dir = "/opt/whatwhy-data/vectorizers"
+
+# model_dir = "/home/stevengt/Documents/code/whatwhy-data/News-Articles/all-the-news/tf-model"
+model_dir = "/opt/whatwhy-data/tf-model"
 
 num_samples = 30000
 max_num_tokens_per_sample = 15
@@ -40,7 +44,7 @@ def create_and_save_token_vectorizers_and_train_and_test_data(what_tokens, why_t
 
 def load_what_why_predictor(vectorizers_dir, model_dir=None):
     w2w_model = WhatWhyPredictor()
-    w2w_model.load_token_vectorizers_from_pickle_files(vectorizers_dir)
+    # w2w_model.load_token_vectorizers_from_pickle_files(vectorizers_dir)
     w2w_model.load_train_and_test_data_from_pickle_files(vectorizers_dir)
     if model_dir is not None:
         w2w_model.load_seq2seq_model_from_saved_tf_model(model_dir)
@@ -48,14 +52,14 @@ def load_what_why_predictor(vectorizers_dir, model_dir=None):
 
 
 
-what_tokens, why_tokens = get_raw_what_and_why_tokens_from_csv(csv_file_name, num_samples)
-create_and_save_token_vectorizers_and_train_and_test_data(what_tokens, why_tokens, max_num_tokens_per_sample, vectorizers_dir)
+# what_tokens, why_tokens = get_raw_what_and_why_tokens_from_csv(csv_file_name, num_samples)
+# create_and_save_token_vectorizers_and_train_and_test_data(what_tokens, why_tokens, max_num_tokens_per_sample, vectorizers_dir)
 
-# w2w_model = load_what_why_predictor(vectorizers_dir)
+w2w_model = load_what_why_predictor(vectorizers_dir)
 # w2w_model = load_what_why_predictor(vectorizers_dir, model_dir)
 
-# w2w_model.fit_tokens(epochs=epochs, batch_size=batch_size)
-# w2w_model.save_model(model_dir)
+w2w_model.fit_tokens(epochs=epochs, batch_size=batch_size)
+w2w_model.save_model(model_dir)
 
 # w2w_model.compare_train_set_to_predictions()
 # predictions = w2w_model.predict_all(what_tokens)
